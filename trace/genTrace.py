7d983d7d344b397da61afdb862bf5a42d6f1a2d7
Baoquan <zhangbaoquan89@gmail.com>
2017-04-19 23:09:12 -0500

    1w2r 1w3r

--- trace/genTrace.py
+++ trace/genTrace.py
@@ -3,14 +3,16 @@ from random import randint
 
 filename = sys.argv[1]
 total_size = int(sys.argv[2]) * 1024 * 1024 * 1024
-sq_lba = 256 * 1024 * 1024 * 2000
+rd_lba = 256 * 1024 * 1024 * 2000
 line_count = 0
 timestamp = 0
 upper = (64 * 4 + 1024) * 256 * 1024 * 1024
 low = 64 * 4 * 256 * 1024 * 1024
-size = 1024 * 3
+size = 1024 * 4
 
 with open(filename, "w") as trace_f:
+    
+    trace_f.write(str(timestamp) + " " + str(rd_lba/512) + " " + str(size) + " 1\n")
 
     while total_size > 0 :
         lba = randint(low, upper)
@@ -26,22 +28,12 @@ with open(filename, "w") as trace_f:
         trace_f.write(str(timestamp) + " " + str(lba/512) + " " + str(size) + " 1\n")
         line_count += 1
  
-        if line_count == 4:
+        if line_count == 1:
             timestep = randint(10, 50)
             timestamp += timestep
+            trace_f.write(str(timestamp) + " " + str(rd_lba/512) + " " + str(size) + " 0\n")
             trace_f.write(str(timestamp) + " " + str(lba/512) + " " + str(size) + " 0\n")
+            trace_f.write(str(timestamp) + " " + str(rd_lba/512) + " " + str(size) + " 0\n")
             line_count += 1
         
-        if line_count == 6:
-            timestep = randint(10, 50)
-            timestamp += timestep
-            trace_f.write(str(timestamp) + " " + str(sq_lba/512) + " " + str(size) + " 1\n")
-            line_count += 1
-        
-        if line_count == 8:
-            timestep = randint(10, 50)
-            timestamp += timestep
-            trace_f.write(str(timestamp) + " " + str(sq_lba/512) + " " + str(size) + " 0\n")
-            line_count += 1
-        
-        line_count = line_count % 10
+        line_count = line_count % 2
