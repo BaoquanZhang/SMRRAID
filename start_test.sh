63eff19d726e1cbb69fd86e8adb95c843eca4d5c
Baoquan <zhangbaoquan89@gmail.com>
2017-04-21 09:03:19 -0500

    1w2r 1w3r vanilla

---
+++ start_test.sh
@@ -0,0 +1,6 @@
+/home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdb -1
+/home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdc -1
+/home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdd -1
+/home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sde -1
+/home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
+./replay config/200GB_2M_1W3R.ini
