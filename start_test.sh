6913ff8974cc71bf918e593c055ba2ab78164a25
Baoquan <zhangbaoquan89@gmail.com>
2017-05-09 15:12:20 -0500

    src22

--- start_test.sh
+++ start_test.sh
@@ -3,4 +3,4 @@
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdd -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sde -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
-./replay config/proj_real_trace.ini
+./replay config/src22.ini
