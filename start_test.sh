acf36f4fffd8f3ec506b17527af7665a4a919fdb
Baoquan <zhangbaoquan89@gmail.com>
2017-06-07 12:26:27 -0500

    vanilla 2MB 50ms-100ms

--- start_test.sh
+++ start_test.sh
@@ -3,4 +3,4 @@
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdd -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sde -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
-./replay config/iosize/200GB_512KB_8W2R.ini
+./replay config/intense/200GB_2M_8W2R_50_100.ini
