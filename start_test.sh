40bd471e69116cde1d03bc03c3096b6ca11188d1
Baoquan <zhangbaoquan89@gmail.com>
2017-06-09 13:38:06 -0500

    idle 2MB 100ms - 150ms

--- start_test.sh
+++ start_test.sh
@@ -3,4 +3,4 @@
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdd -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sde -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
-./replay config/intense/200GB_2MB_8W2R_50_100.ini
+./replay config/intense/200GB_2MB_8W2R_100_150.ini
