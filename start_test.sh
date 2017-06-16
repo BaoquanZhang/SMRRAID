cbb3943124e6ba1ae23fffce2f9a2124d3485910
Baoquan <zhangbaoquan89@gmail.com>
2017-06-16 09:03:15 -0500

    7disk 150ms 200ms

--- start_test.sh
+++ start_test.sh
@@ -4,4 +4,5 @@
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sde -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdg -1
-./replay config/scal/200GB_2_5M_8W2R_6disk.ini
+/home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdh -1
+./replay config/scal/200GB_3M_8W2R_7disk.ini
