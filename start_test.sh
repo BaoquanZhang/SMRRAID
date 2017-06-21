a61dd9c2dca9ccf8f23d2c982023cb41ae47f735
Baoquan <zhangbaoquan89@gmail.com>
2017-06-21 09:19:29 -0500

    30s

--- start_test.sh
+++ start_test.sh
@@ -5,4 +5,4 @@
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdg -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdh -1
-./replay config/idletime/200GB_2MB_8W2R_120s_50_100.ini
+./replay config/idletime/200GB_2MB_8W2R_30s_50_100.ini
