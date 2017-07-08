585f14475d505a102910e452444c6a49b8950156
Baoquan <zhangbaoquan89@gmail.com>
2017-07-08 00:36:06 -0500

    50ms 80% vanilla

--- start_test.sh
+++ start_test.sh
@@ -5,4 +5,4 @@
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdf -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdg -1
 /home/bzhang/tests/libzbc/zbc_reset_write_ptr /dev/sdh -1
-./replay config/scal/200GB_3M_8W2R_7disk.ini
+./replay config/slowtier/50ms_80/50ms_80.ini
